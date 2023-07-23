238f8ab93e84b1e7f8240b70de6efc5e49346ed0
Hridoy Roy <royhridoy1428@gmail.com>
2023-07-23 11:11:51 +0600

    Update data.js
    Some Questions added

--- src/data/data.js
+++ src/data/data.js
@@ -91,4 +91,130 @@ export const questions = [
             }
         ]
     },
+    {
+        question: 'What is the multiplication of 35 and 4 ?',
+        options: [
+            {
+                option: 170,
+                isCorrect: 0
+            },
+            {
+                option: 90,
+                isCorrect: 0
+            },
+            {
+                option: 140,
+                isCorrect: 1
+            },
+            {
+                option: 135,
+                isCorrect: 0
+            }
+        ]
+    },
+    {
+        question: 'What is the capital of France ?',
+        options: [
+            {
+                option: 'Paris',
+                isCorrect: 1
+            },
+            {
+                option: 'Birlin',
+                isCorrect: 0
+            },
+            {
+                option: 'Dhaka',
+                isCorrect: 0
+            },
+            {
+                option: 'Doha',
+                isCorrect: 0
+            }
+        ]
+    },
+    {
+        question: ' (3+2) * 4 + 5 ?',
+        options: [
+            {
+                option: 45,
+                isCorrect: 0
+            },
+            {
+                option: 36,
+                isCorrect: 0
+            },
+            {
+                option: 29,
+                isCorrect: 0
+            },
+            {
+                option: 25,
+                isCorrect: 1
+            }
+        ]
+    },
+    {
+        question: 'Which country won FIFA WORLD CUP 2022?',
+        options: [
+            {
+                option: 'Brazil',
+                isCorrect: 0
+            },
+            {
+                option: 'France',
+                isCorrect: 0
+            },
+            {
+                option: 'Argentina',
+                isCorrect: 1
+            },
+            {
+                option: 'Germany',
+                isCorrect: 0
+            }
+        ]
+    },
+    {
+        question: 'This Year is __?',
+        options: [
+            {
+                option: 2021,
+                isCorrect: 0
+            },
+            {
+                option: 2022,
+                isCorrect: 0
+            },
+            {
+                option: (new Date()).getFullYear(),
+                isCorrect: 1
+            },
+            {
+                option: 2020,
+                isCorrect: 0
+            }
+        ]
+    },
+    {
+        question: 'How many letter are there in \'Apple\' ?',
+        options: [
+            {
+                option: 4,
+                isCorrect: 0
+            },
+            {
+                option: 5,
+                isCorrect: 1
+            },
+            {
+                option: 6,
+                isCorrect: 0
+            },
+            {
+                option: 7,
+                isCorrect: 0
+            }
+        ]
+    },
 ]
