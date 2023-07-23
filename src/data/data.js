58187f07abf9a0fb4612dca84c670f2fb0dd9ebd
royHridoy <royhridoy1428@gmail.com>
2023-07-23 10:40:34 +0600

    Initial Release

---
+++ src/data/data.js
@@ -0,0 +1,94 @@
+export const questions = [
+    {
+        question: 'Fill in the blanks: He is __ honest person.',
+        options: [
+            {
+                option: 'a',
+                isCorrect: 0
+            },
+            {
+                option: 'an',
+                isCorrect: 1
+            },
+            {
+                option: 'the',
+                isCorrect: 0
+            },
+            {
+                option: 'No article',
+                isCorrect: 0
+            }
+        ]
+    },
+    {
+        question: 'Fill in the blanks: Ani__al.',
+        options: [
+            {
+                option: 'a',
+                isCorrect: 0
+            },
+            {
+                option: 'm',
+                isCorrect: 1
+            },
+            {
+                option: 'e',
+                isCorrect: 0
+            },
+            {
+                option: 'd',
+                isCorrect: 0
+            },
+            {
+                option: 'No of these',
+                isCorrect: 0
+            }
+        ]
+    },
+    {
+        question: 'Bangladesh achieved independence in __ ?',
+        options: [
+            {
+                option: 1970,
+                isCorrect: 0
+            },
+            {
+                option: 1971,
+                isCorrect: 1
+            },
+            {
+                option: 1966,
+                isCorrect: 0
+            },
+            {
+                option: 1947,
+                isCorrect: 0
+            },
+            {
+                option: 1952,
+                isCorrect: 0
+            }
+        ]
+    },
+    {
+        question: 'What is the summation of 35 and 45 ?',
+        options: [
+            {
+                option: 70,
+                isCorrect: 0
+            },
+            {
+                option: 90,
+                isCorrect: 0
+            },
+            {
+                option: 80,
+                isCorrect: 1
+            },
+            {
+                option: 75,
+                isCorrect: 0
+            }
+        ]
+    },
+]
